bd59447b8a6a0e91022dc69f8b658be7336f1e5c
Tammy Lam <lamtammy1515@gmail.com>
2019-12-13 19:29:04 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,8 +1,5 @@
-# birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
-#   "Amanda" => 27
-# }
+
+
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
@@ -11,5 +8,19 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
+
+def age_appropriate_birthday(array)
+  
+  age_appropriate_birthday.each 
+    
+  end
+end
+
+
+
+
+
+
+
 
 
